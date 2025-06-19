4943541a167b516f80e47b9d7348a3ea33c9308d
nilton-medeiros <nilton@sistrom.com.br>
2025-06-19 15:52:41 -0300

    refactor: Alterado import para apontar para o novo sistema de log

--- src/shared/config/__init__.py
+++ src/shared/config/__init__.py
@@ -1,2 +1,5 @@
 from .get_app_colors import get_app_colors
-from .logging_config import LogConfig
+# A configuração de logging agora acontece quando logging_config.py é importado,
+# que é acionado pela linha abaixo.
+# Não há mais uma classe LogConfig para exportar diretamente daqui.
+import src.shared.config.logging_config
